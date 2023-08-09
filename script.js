caa264afe1500db4f2cd97da9bb94ff2d8524d83
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-08-09 22:45:16 +0600

    fetures adding

--- script.js
+++ script.js
@@ -1,80 +1,109 @@
-const taskInput = document.getElementById('taskInput');
-const addButton = document.getElementById('addButton');
-const taskList = document.getElementById('taskList');
+// Get references to HTML elements
+const taskInput = document.getElementById("taskInput");
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
 
-addButton.addEventListener('click', addTask);
+// Add event listener to "Add" button
+addButton.addEventListener("click", addTask);
 
-taskInput.addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
+// Add event listener for keyboard inputs on the task input field
+taskInput.addEventListener("keyup", function (event) {
+  // If Enter key is pressed, add the task
+  if (event.key === "Enter") {
     addTask();
-  } else if (event.key === 'Escape') {
+  }
+  // If Escape key is pressed, clear the input
+  else if (event.key === "Escape") {
     clearInput();
   }
 });
 
+// Function to add a new task
 function addTask() {
   const taskText = taskInput.value.trim();
-  if (taskText !== '') {
+  if (taskText !== "") {
+    // Call function to create task element
     createTaskElement(taskText);
-    taskInput.value = '';
+    // Clear the task input field
+    taskInput.value = "";
   }
 }
 
+// Function to clear the task input field
 function clearInput() {
-  taskInput.value = '';
+  taskInput.value = "";
 }
 
+// Function to create a new task element
 function createTaskElement(taskText) {
-  const taskItem = document.createElement('li');
-  taskItem.classList.add('task-item');
-  
-  const taskContent = document.createElement('span');
+  // Create a new list item for the task
+  const taskItem = document.createElement("li");
+  taskItem.classList.add("task-item");
+
+  // Create a span for the task content
+  const taskContent = document.createElement("span");
   taskContent.textContent = taskText;
-  
-  const buttonsDiv = document.createElement('div');
-  buttonsDiv.classList.add('buttons');
-  
-  const doneButton = document.createElement('button');
-  doneButton.textContent = 'Done';
-  doneButton.className = 'done-button';
-  doneButton.addEventListener('click', function() {
-    taskItem.classList.toggle('done');
+
+  // Create a div for the task buttons
+  const buttonsDiv = document.createElement("div");
+  buttonsDiv.classList.add("buttons");
+
+  // Create the "Done" button
+  const doneButton = document.createElement("button");
+  doneButton.textContent = "Done";
+  doneButton.className = "done-button";
+  doneButton.addEventListener("click", function () {
+    // Toggle the "done" class on the task item
+    taskItem.classList.toggle("done");
+    // Update the "Date & Time" button text
     updateDateTimeButton(taskItem, taskText);
   });
 
-  const dateTimeButton = document.createElement('button');
-  dateTimeButton.textContent = 'Date & Time';
-  dateTimeButton.className = 'date-time-button';
-  dateTimeButton.addEventListener('click', function() {
-    const dateTimeMessage = taskItem.classList.contains('done')
-      ? 'This work was done.'
-      : 'This work is not done yet.';
-    
+  // Create the "Date & Time" button
+  const dateTimeButton = document.createElement("button");
+  dateTimeButton.textContent = "Date & Time";
+  dateTimeButton.className = "date-time-button";
+  dateTimeButton.addEventListener("click", function () {
+    // Determine whether the task is done or not
+    const dateTimeMessage = taskItem.classList.contains("done")
+      ? "This work was done."
+      : "This work is not done yet.";
+    // Get the current date and time
     const dateTime = new Date();
+    // Show an alert with the appropriate message and timestamp
     alert(`${dateTimeMessage} Marked on ${dateTime.toLocaleString()}`);
   });
-  
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-  deleteButton.className = 'delete';
-  deleteButton.addEventListener('click', function() {
+
+  // Create the "Delete" button
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.addEventListener("click", function () {
+    // Remove the task item from the list
     taskList.removeChild(taskItem);
   });
-  
+
+  // Append buttons to the buttons div
   buttonsDiv.appendChild(doneButton);
   buttonsDiv.appendChild(dateTimeButton);
   buttonsDiv.appendChild(deleteButton);
-  
+
+  // Append task content and buttons div to the task item
   taskItem.appendChild(taskContent);
   taskItem.appendChild(buttonsDiv);
+
+  // Append the task item to the task list
   taskList.appendChild(taskItem);
 }
 
+// Function to update the "Date & Time" button text
 function updateDateTimeButton(taskItem, taskText) {
-  const dateTimeButton = taskItem.querySelector('.date-time-button');
-  const dateTimeMessage = taskItem.classList.contains('done')
+  const dateTimeButton = taskItem.querySelector(".date-time-button");
+  // Determine the appropriate message based on task completion status
+  const dateTimeMessage = taskItem.classList.contains("done")
     ? `This work "${taskText}" was done.`
     : `This work "${taskText}" is not done yet.`;
-  
+
+  // Update the button text
   dateTimeButton.textContent = dateTimeMessage;
 }
